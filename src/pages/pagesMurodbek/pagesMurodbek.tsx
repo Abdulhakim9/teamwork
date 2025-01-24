975e75b5be497a6903509837a88d4ea2fc68a628
Murodbek <gulmatovmurodbek5@gmail.com>
2025-01-24 04:47:16 +0300

    Ma shim

--- src/pages/pagesMurodbek/pagesMurodbek.tsx
+++ src/pages/pagesMurodbek/pagesMurodbek.tsx
@@ -1,8 +1,181 @@
-import React from 'react'
-
+import './pagesMurodbek.scss'
+import surat1 from './imagesMurod/фон.png'
+import surat2 from './imagesMurod/Ватсап (1).png'
 const PagesMurodbek = () => {
   return (
-    <div>PagesMurodbek</div>
+    <div className='conteyner'>
+      <div className='header'>
+        <div className='under'>
+          <p className='about'>Главная  /  Распродажи</p>
+          <h1 className='h1'>Распродажи</h1>
+          <p className='p'>Ранний доступ к распродажам в магазинах</p>
+          <button className='button'>Получить адрес в UK</button>
+        </div>
+      </div>
+      <div className='section2'>
+        <div className='divH1'>
+          <h1 className=''>Black Friday Early Access
+            ранний доступ к распродажам
+            в магазинах Великобритании!</h1>
+        </div>
+        <div className='divBig'>
+          <div className='divProduct'>
+            <div>
+              <img src={surat1} alt="" />
+            </div>
+            <div className='divLeft'>
+              <h4>Abercrombie & Fitch</h4>
+              <p className="">цены от £7.99 (3365₽) разделе:  <br />
+                мужские вещи: <span className='span'>CLEARANCE</span> 
+                <br />
+                женские вещи: <span className='span'>CLEARANCE</span> 
+              </p>
+              <p>качественные детские вещи<br />
+              <span className='span'>
+                на Abercrombie kids</span></p>
+              <p className='span'>Только выкуп с оператором →</p>
+            </div>
+          </div>
+          <div className='divProduct'>
+            <div>
+              <img src={surat1} alt="" />
+            </div>
+            <div className='divLeft'>
+              <h4>Abercrombie & Fitch</h4>
+              <p className="">цены от £7.99 (3365₽) разделе:  <br />
+                мужские вещи: <span className='span'>CLEARANCE</span> 
+                <br />
+                женские вещи: <span className='span'>CLEARANCE</span> 
+              </p>
+              <p>качественные детские вещи<br />
+              <span className='span'>
+                на Abercrombie kids</span></p>
+              <p className='span'>Только выкуп с оператором →</p>
+            </div>
+          </div>
+          <div className='divProduct'>
+            <div>
+              <img src={surat1} alt="" />
+            </div>
+            <div className='divLeft'>
+              <h4>Abercrombie & Fitch</h4>
+              <p className="">цены от £7.99 (3365₽) разделе:  <br />
+                мужские вещи: <span className='span'>CLEARANCE</span> 
+                <br />
+                женские вещи: <span className='span'>CLEARANCE</span> 
+              </p>
+              <p>качественные детские вещи<br />
+              <span className='span'>
+                на Abercrombie kids</span></p>
+              <p className='span'>Только выкуп с оператором →</p>
+            </div>
+          </div>
+          <div className='divProduct'>
+            <div>
+              <img src={surat1} alt="" />
+            </div>
+            <div className='divLeft'>
+              <h4>Abercrombie & Fitch</h4>
+              <p className="">цены от £7.99 (3365₽) разделе:  <br />
+                мужские вещи: <span className='span'>CLEARANCE</span> 
+                <br />
+                женские вещи: <span className='span'>CLEARANCE</span> 
+              </p>
+              <p>качественные детские вещи<br />
+              <span className='span'>
+                на Abercrombie kids</span></p>
+              <p className='span'>Только выкуп с оператором →</p>
+            </div>
+          </div>
+          <div className='divProduct'>
+            <div>
+              <img src={surat1} alt="" />
+            </div>
+            <div className='divLeft'>
+              <h4>Abercrombie & Fitch</h4>
+              <p className="">цены от £7.99 (3365₽) разделе:  <br />
+                мужские вещи: <span className='span'>CLEARANCE</span> 
+                <br />
+                женские вещи: <span className='span'>CLEARANCE</span> 
+              </p>
+              <p>качественные детские вещи<br />
+              <span className='span'>
+                на Abercrombie kids</span></p>
+              <p className='span'>Только выкуп с оператором →</p>
+            </div>
+          </div>
+          <div className='divProduct'>
+            <div>
+              <img src={surat1} alt="" />
+            </div>
+            <div className='divLeft'>
+              <h4>Abercrombie & Fitch</h4>
+              <p className="">цены от £7.99 (3365₽) разделе:  <br />
+                мужские вещи: <span className='span'>CLEARANCE</span> 
+                <br />
+                женские вещи: <span className='span'>CLEARANCE</span> 
+              </p>
+              <p>качественные детские вещи<br />
+              <span className='span'>
+                на Abercrombie kids</span></p>
+              <p className='span'>Только выкуп с оператором →</p>
+            </div>
+          </div>
+          <div className='divProduct'>
+            <div>
+              <img src={surat1} alt="" />
+            </div>
+            <div className='divLeft'>
+              <h4>Abercrombie & Fitch</h4>
+              <p className="">цены от £7.99 (3365₽) разделе:  <br />
+                мужские вещи: <span className='span'>CLEARANCE</span> 
+                <br />
+                женские вещи: <span className='span'>CLEARANCE</span> 
+              </p>
+              <p>качественные детские вещи<br />
+              <span className='span'>
+                на Abercrombie kids</span></p>
+              <p className='span'>Только выкуп с оператором →</p>
+            </div>
+          </div>
+          <div className='divProduct'>
+            <div>
+              <img src={surat1} alt="" />
+            </div>
+            <div className='divLeft'>
+              <h4>Abercrombie & Fitch</h4>
+              <p className="">цены от £7.99 (3365₽) разделе:  <br />
+                мужские вещи: <span className='span'>CLEARANCE</span> 
+                <br />
+                женские вещи: <span className='span'>CLEARANCE</span> 
+              </p>
+              <p>качественные детские вещи<br />
+              <span className='span'>
+                на Abercrombie kids</span></p>
+              <p className='span'>Только выкуп с оператором →</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='end'>
+        <div className='under2'>
+          <h1 className='about2'>Остались вопросы? Спросите нас в соцсетях!</h1>
+          <div className='images'>
+            <div>
+              <img src={surat2} alt="" />
+            </div>
+            <div>
+              <img src={surat2} alt="" />
+            </div>
+            <div>
+              <img src={surat2} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
